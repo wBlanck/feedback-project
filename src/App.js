9896491dd405b9e543b5ee79c9c2e0cd7a61ec61
wblanck <blanck.william@gmail.com>
2022-01-19 16:47:03 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
